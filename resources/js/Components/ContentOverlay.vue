3f255dd57b2c9848b88d03caba8e21dad3bfe20a
Vishal78626 <vishalgupta78626@gmail.com>
2023-10-14 12:17:16 +0530

    using vueJs and laravel

---
+++ resources/js/Components/ContentOverlay.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, toRefs } from 'vue';
+
+import Heart from "vue-material-design-icons/Heart.vue";
+import Comment from "vue-material-design-icons/Comment.vue";
+
+defineEmits(['selectedPost'])
+const props = defineProps(['postsByUser']);
+const { postsByUser } = toRefs(props);
+
+// console.log(JSON.stringify(postsByUser))
+let isHover = ref(false)
+</script>
+
+<template>
+    <div @mouseenter="isHover = true" @mouseleave="isHover = false"
+        class="flex items-center justify-center cursor-pointer relative"
+        @click="$emit('selectedPost', postsByUser)"
+        >
+
+        <div v-if="isHover" :class="isHover ? 'bg-black bg-opacity-40' : '' "
+            class=" absolute w-full h-full z-50 flex items-center justify-around text-lg font-extrabold text-white">
+            <div class=" flex items-center justify-around w-[50%]">
+                <div class="flex items-center justify-center">
+                    <Heart :size="30" fillColor="#FFFFFF" />
+                    <div class="pl-1">{{ postsByUser.likes.length }}</div>
+                </div>
+                <div class="flex items-center justify-center">
+                    <Comment :size="30" fillColor="#FFFFFF" />
+                    <div class="pl-1">{{ postsByUser.comments.length }}</div>
+                </div>
+            </div>
+        </div>
+
+        <img class="aspect-square mx-auto z-0 object-cover cursor-pointer" :src="postsByUser.file" />
+    </div>
+</template>
